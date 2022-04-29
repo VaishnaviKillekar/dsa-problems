e84b1f5981ab58982444631026db58a651201f90
Vaishnavi Killekar <vaishnavikillekar@gmail.com>
2022-04-29 08:52:25 +0530

    Create Merge Sorted Array.java

---
+++ Arrays/Merge Sorted Array.java
@@ -0,0 +1,36 @@
+// Link to problem - https://leetcode.com/problems/merge-sorted-array/
+
+/**
+ * Intuition - Start scanning both arrays backward as they are already sorted.
+ * Compare elements of the arrays and the larger one must be added to the end of the larger array.
+ *
+ * Time complexity - O(m+n)
+ * Space complexity - o(1)
+ */
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p = m - 1;
+        int q = n - 1;
+        int i = nums1.length - 1;
+        
+        while(p >= 0 && q >= 0) {
+            if(nums1[p] > nums2[q]) {
+                nums1[i--] = nums1[p--];
+            }
+            else {
+                nums1[i--] = nums2[q--];
+            }
+        }
+        
+        if(p >= 0) {
+            while(p >= 0) {
+                nums1[i--] = nums1[p--];
+            }
+        }
+        else {
+            while(q >= 0) {
+                nums1[i--] = nums2[q--];
+            }
+        }
+    }
+}
