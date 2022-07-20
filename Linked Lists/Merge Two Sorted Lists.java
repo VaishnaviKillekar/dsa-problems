119be56493fa0580694a77284acca202e4f8d430
Vaishnavi Killekar <vaishnavikillekar@gmail.com>
2022-07-20 07:54:59 +0530

    Added shorter solution

--- Linked Lists/Merge Two Sorted Lists.java
+++ Linked Lists/Merge Two Sorted Lists.java
@@ -8,16 +8,30 @@
  * Time complexity - O(n)
  * Space complexity - O(1)
  */
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        
+        while(list1 != null && list2 != null) {
+            if(list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            }
+            else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        
+        curr.next = list1 != null ? list1 : list2;
+        
+        return dummy.next;
+    }
+}
+
+// Long solution - do not use this
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode head = null;
@@ -52,16 +66,12 @@ class Solution {
             curr = curr.next;
         }
         
-        while(list1 != null) {
+        if(list1 != null) {
             curr.next = list1;
-            list1 = list1.next;
-            curr = curr.next;
         }
         
-        while(list2 != null) {
+        if(list2 != null) {
             curr.next = list2;
-            list2 = list2.next;
-            curr = curr.next;
         }
         
         return head;
