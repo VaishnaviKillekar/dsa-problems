a5b5c59013cc4844e36959060b7da258e6848c62
Vaishnavi Killekar <vaishnavikillekar@gmail.com>
2022-07-26 08:47:14 +0530

    XOR approach

---
+++ Bit Manipulation/Single Number.java
@@ -0,0 +1,19 @@
+// Link to problem - https://leetcode.com/problems/single-number/
+
+/**
+ * Intuition - Do an XOR of all array elements. All pairs cancel each other - A ^ A => 0
+ * Only the unique element will remain.
+ *
+ * Time complexity - O(n)
+ * Space complexity - O(1)
+ */
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum = sum ^ nums[i];
+        }
+        return sum;
+    }
+}
