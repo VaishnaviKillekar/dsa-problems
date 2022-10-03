e81e8676cf64971bab61f069b7f500dee9cda1c2
Vaishnavi Killekar <vaishnavikillekar@gmail.com>
2022-10-03 18:41:02 +0530

    Pretty solution added

--- Strings/Valid Palindrome.java
+++ Strings/Valid Palindrome.java
@@ -1,6 +1,36 @@
 // Link to problem - https://leetcode.com/problems/valid-palindrome/
 
 /**
+ * Concise, prettier solution
+ *
+ * Time complexity - O(n)
+ * Space complexity - O(1)
+ */
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while(left < right) {
+            while(left < s.length() && !(Character.isDigit(s.charAt(left)) || Character.isLetter(s.charAt(left)))) {
+                left++;
+            }
+            while(right > 0 && !(Character.isDigit(s.charAt(right)) || Character.isLetter(s.charAt(right)))) {
+                right--;
+            }
+            
+            if(left < right && Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
+
+/**
  * Intuition - Iterate through the string and compare only alphanumeric characters using 'left' and
  * 'right' pointers.
  * Skip over characters that are not alphanumeric and increment or decrement the pointer based on it.
