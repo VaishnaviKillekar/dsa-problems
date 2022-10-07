7584ba3cd83d472fb429edca5d5aefb53a955f9b
Vaishnavi Killekar <vaishnavikillekar@gmail.com>
2022-10-07 07:52:52 +0530

    Optimised code

--- Linked Lists/Linked List Cycle.java
+++ Linked Lists/Linked List Cycle.java
@@ -1,31 +1,24 @@
-// // Link to problem - https://leetcode.com/problems/linked-list-cycle/
+// Link to problem - https://leetcode.com/problems/linked-list-cycle/
 
 /**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
+ * Intuition - Use a fast and slow pointer to find the cycle.
+ * If there's a cycle, then the fast and slow pointer meet at a node.
+ *
+ * Time complexity - O(n)
+ * Space complexity - O(1)
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null) {
-            return false;
-        }
-        
         ListNode slow = head;
-        ListNode fast = head.next;
+        ListNode fast = head;
         
-        while(slow.next != null && fast != null && fast.next != null && !fast.next.equals(slow)) {
+        while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
+            if(slow == fast) {
+                return true;
+            }
         }
-        if(fast != null && fast.next != null && fast.next.equals(slow))
-            return true;
         return false;
     }
 }
