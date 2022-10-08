ce29a6c611c4d7d2d7a9c59fb7e26408b3e2e3b9
Vaishnavi Killekar <vaishnavikillekar@gmail.com>
2022-10-08 09:31:23 +0530

    Concise solution added

--- Dynamic Programming/Best Time to Buy and Sell Stock.java
+++ Dynamic Programming/Best Time to Buy and Sell Stock.java
@@ -1,6 +1,31 @@
 // Link to problem - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
 /**
+ * Concise solution
+ *
+ * Time complexity - O(n)
+ * Space complexity - O(1)
+ */
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int low = 0;
+        
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] - prices[low] > profit) {
+                profit = prices[i] - prices[low];
+            }
+            if(prices[i] < prices[low]) {
+                low = i;
+            }
+        }
+        
+        return profit;
+    }
+}
+
+
+/**
  * Intuition - If there are only two elements in the array, then the max profit is prices[1] - prices[0], if prices[1] > prices[0]. Otherwise, 0.
  * Maintain a minimum element 'min' which is the lowest one read so far.
  * After scanning a new element, compare its difference with min and existing max profit. If it is higher, update the max profit.
