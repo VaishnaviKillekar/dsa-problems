2322f65a8ec2ee0a34e1b556cce0b853c33f264e
Vaishnavi Killekar <vaishnavikillekar@gmail.com>
2023-01-02 09:56:35 +0530

    BFS solution

--- Strings/Letter Combinations of a Phone Number.java
+++ Strings/Letter Combinations of a Phone Number.java
@@ -1,6 +1,37 @@
 // Link to problem - https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 
 /**
+ * Intuition - BFS
+ * Start with an empty linked list and continue appending characters for current digit to the first
+ * combinations in the result 'combo' by removing it. Continue this until the length of the first
+ * string combination in 'combo' is not equal to the number of digits.
+ *
+ * Time complexity - O(n^2)
+ * Space complexity - O(1)
+ */
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        String[] map = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        LinkedList<String> combo = new LinkedList<>();
+
+        if(digits.length() == 0) {
+            return combo;
+        }
+
+        combo.add("");
+        while(combo.peek().length() != digits.length()) {
+            String prefix = combo.remove();
+            String suffix = map[digits.charAt(prefix.length()) - '0'];
+            for(char c : suffix.toCharArray()) {
+                combo.add(prefix + c);
+            }
+        }
+
+        return combo;
+    }
+}
+
+/**
  * Intuition - Start with alphabets from the first digit and add them to the result.
  * Iterate through the result and append each aphabet of the next digit with all the 
  * alphabets in result.
