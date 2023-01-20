44f78b2426721cc95858afc6a78cddc8b7c010e7
Vaishnavi Killekar <vaishnavikillekar@gmail.com>
2023-01-20 08:21:06 +0530

    Solution which updates given matrix

--- Arrays/SpiralMatrix.java
+++ Arrays/SpiralMatrix.java
@@ -1,5 +1,13 @@
 // Link to problem - https://leetcode.com/problems/spiral-matrix/
 
+/**
+ * Intuition - Using bounding variables to track scanned rows and columns
+ * Track the scanned rows and columns using top, bottom, left and right variables.
+ * Iterate through the rows and columns within these bounding variables.
+ *
+ * Time complexity - O(mn)
+ * Space complexity - O(1)
+ */
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> order = new ArrayList();
@@ -49,6 +57,68 @@ class Solution {
             i++;
             j++;
         }
+        return order;
+    }
+}
+
+
+/**
+ * Intuition - Marking scanned elements
+ * Scan through the matrix in all for directions in a sequence using the element value -1000
+ * to identify if the element is already scanned.
+ * First left to right -> top to bottom -> right to left -> bottom to top
+ *
+ * Time complexity - O(mn)
+ * Space complexity - O(1)
+ */
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> order = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+
+        while(i < matrix.length && j < matrix[0].length && matrix[i][j] != -1000) {
+            // Scan left to right
+            while(j < matrix[0].length && matrix[i][j] != -1000) {
+                order.add(matrix[i][j]);
+                matrix[i][j] = -1000;
+                j++;
+            }
+            // Move pointer to the next element vertically
+            j--;
+            i++;
+
+            // Scan top to bottom
+            while(i < matrix.length && matrix[i][j] != -1000) {
+                order.add(matrix[i][j]);
+                matrix[i][j] = -1000;
+                i++;
+            }
+            // Move pointer to the next element horizontally
+            i--;
+            j--;
+
+            // Scan right to left
+            while(j >= 0 && matrix[i][j] != -1000) {
+                order.add(matrix[i][j]);
+                matrix[i][j] = -1000;
+                j--;
+            }
+            // Move pointer to the next element vertically
+            i--;
+            j++;
+
+            // Scan bottom to top
+            while(i >= 0 && matrix[i][j] != -1000) {
+                order.add(matrix[i][j]);
+                matrix[i][j] = -1000;
+                i--;
+            }
+            // Move pointer to the next element horizontally
+            j++;
+            i++;
+        }
+
         return order;
     }
 }
