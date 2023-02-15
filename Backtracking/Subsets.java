e1f08cb43679c81d71a4a208b62893339e98d9e8
Vaishnavi Killekar <vaishnavikillekar@gmail.com>
2023-02-15 08:44:41 +0530

    Solution updated

--- Backtracking/Subsets.java
+++ Backtracking/Subsets.java
@@ -1,5 +1,32 @@
 // Link to problem - https://leetcode.com/problems/subsets/
 
+/**
+ * Intuition - Backtracking [ Concise solution ]
+ * Use index to track the current element to be added. Since subsets are unique, only add elements
+ * to 'curr' whose index is higher than current index to avoid permutations of already added subsets.
+ *
+ * Time complexity - O(2^n) - since total count of all subsets is 2^n 
+ * Space complexity - O(n) - recursion tree at the max will have n levels. 'curr' at the max will hold all n elements.
+ */
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        generateSubsets(nums, res, new ArrayList<>(), 0);
+        return res;
+    }
+
+    public void generateSubsets(int[] nums, List<List<Integer>> res, List<Integer> curr, int index) {
+        res.add(new ArrayList<>(curr));
+
+        for(int i = index; i < nums.length; i++) {
+            curr.add(nums[i]);
+            generateSubsets(nums, res, curr, i + 1);
+            curr.remove(curr.size() - 1);
+        }
+    }
+}
+
+
 /** 
  * Intuition - Start with an empty array and use recursion to generate all subsets
  *             [ ]
