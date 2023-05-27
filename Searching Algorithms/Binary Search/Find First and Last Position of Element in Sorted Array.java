a08bf5994ed29399b8c011a7a99e04475b5780bf
Vaishnavi Killekar <vaishnavikillekar@gmail.com>
2023-05-27 08:58:43 +0530

    Efficient solution

--- Searching Algorithms/Binary Search/Find First and Last Position of Element in Sorted Array.java
+++ Searching Algorithms/Binary Search/Find First and Last Position of Element in Sorted Array.java
@@ -1,3 +1,80 @@
+// Link to problem - https://www.codingninjas.com/codestudio/problems/first-and-last-position-of-an-element-in-sorted-array_1082549
+
+/**
+ * Intuition - Use Binary Search to search once only the left side of the array once an occurrence
+ * of key is found and then search to the right of array once an occurrence is found.
+ *
+ * Time complexity - O(logn)
+ * Space complexity - O(1)
+ */
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+
+    public static int[] firstAndLastPosition(ArrayList<Integer> arr, int n, int k) {
+        int first = -1;
+        int last = -1;
+        int left = 0;
+        int right = n - 1;
+        int[] result = new int[2];
+
+        first = searchLeft(arr, k, 0, n - 1);
+        last = searchRight(arr, k, 0, n - 1);
+        result[0] = first;
+        result[1] = last;
+
+        if(first == -1 || last == -1) {
+            result[0] = Math.max(first, last);
+            result[1] = Math.max(first, last);
+        }
+
+        return result;
+    }
+
+    public static int searchLeft(ArrayList<Integer> arr, int key, int left, int right) {
+        int first = -1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(arr.get(mid) == key) {
+                first = mid;
+                right = mid - 1;
+            }
+            else if(arr.get(mid) > key) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return first;
+    }
+
+    public static int searchRight(ArrayList<Integer> arr, int key, int left, int right) {
+        int last = -1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(arr.get(mid) == key) {
+                last = mid;
+                left = mid + 1;
+            }
+            else if(arr.get(mid) > key) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return last;
+    }
+};
+
+
+// LeetCode
+
 // Link to problem - https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
 
 /**
