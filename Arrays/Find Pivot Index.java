4225b21453fe2b4075c159df93705ffe5385a93f
Vaishnavi Killekar <vaishnavikillekar@gmail.com>
2023-05-27 10:07:44 +0530

    Concise solution

--- Arrays/Find Pivot Index.java
+++ Arrays/Find Pivot Index.java
@@ -1,5 +1,41 @@
 // Link to problem - https://leetcode.com/problems/find-pivot-index/
 
+/**
+ * Intuition - Concise linear search
+ * First find the sum of elements except the first and that forms the rightmost sum 'right'.
+ * Initially leftmost sum 'left' is zero indicating the left of array with 0th element as
+ * pivot. Scan the array and compare 'left' and 'right'. If they are equal, return current
+ * index as pivot. Otherwise, increment 'left' using current element and decrement 'right'
+ * by next element so as to move the pivot to next element.
+ *
+ * Time complexity - O(n)
+ * Space complexity - O(1)
+ */
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int left = 0;   // Sum of leftmost elements
+        int right = 0;      // Sum of rightmost elements
+
+        for(int i = 1; i < nums.length; i++) {
+            right += nums[i];
+        }
+
+        // Iterate through array and compare left and right sum
+        for(int i = 0; i < nums.length; i++) {
+            if(left == right) {
+                return i;
+            }
+            else {
+                left += nums[i];
+                right = i < nums.length - 1 ? right - nums[i + 1] : 0;
+            }
+        }
+
+        return -1;
+    }
+}
+
+
 class Solution {
     public int pivotIndex(int[] nums) {
         int left = 0;
