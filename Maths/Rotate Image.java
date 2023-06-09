402269053da8dfa9c2b68576dcc18e7740010130
Vaishnavi Killekar <vaishnavikillekar@gmail.com>
2023-06-09 09:48:56 +0530

    Different approach

--- Maths/Rotate Image.java
+++ Maths/Rotate Image.java
@@ -1,6 +1,41 @@
 // Link to problem - https://leetcode.com/problems/rotate-image/
 
 /**
+ * Intuition - Transpose the matrix and then reverse elements in each row.
+ *
+ * Time complexity - O(n^2)
+ * Space complexity - O(1)
+ */
+class Solution {
+    public void rotate(int[][] matrix) {
+        // Transpose the given matrix
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = i; j < matrix[0].length; j++) {
+                if(i != j) {
+                    int t = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = t;
+                }
+            }
+        }
+
+        // Reverse the rows of transposed matrix
+        for(int i = 0; i < matrix.length; i++) {
+            int j = 0;
+            int k = matrix[0].length - 1;
+            while(j < k){
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[i][k];
+                matrix[i][k] = t;
+                j++;
+                k--;
+            }
+        }
+    }
+}
+
+
+/**
  * Intuition - Flip the matrix upside down and then transpose it.
  *
  * Time complexity - O(n)
