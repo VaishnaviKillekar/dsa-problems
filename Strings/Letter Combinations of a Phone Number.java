8d4ceb7300c0b8d4312e6fa165be606143d1e9a8
Vaishnavi Killekar <vaishnavikillekar@gmail.com>
2023-06-20 09:11:18 +0530

    Updated code with complexity.java

--- Strings/Letter Combinations of a Phone Number.java
+++ Strings/Letter Combinations of a Phone Number.java
@@ -1,35 +1,43 @@
 // Link to problem - https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 
 /**
- * Intuition - BFS
+ * Intuition - BFS (Iterative)
  * Start with an empty linked list and continue appending characters for current digit to the first
  * combinations in the result 'combo' by removing it. Continue this until the length of the first
  * string combination in 'combo' is not equal to the number of digits.
  *
- * Time complexity - O(n^2)
- * Space complexity - O(1)
+ * Time complexity - O(4^n) - n is the length of digits and each digit can have a maximum of 4 letters.
+ * Space complexity - O(n) - could be constant
  */
 class Solution {
     public List<String> letterCombinations(String digits) {
-        String[] map = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        LinkedList<String> combo = new LinkedList<>();
+        String[] letters = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        LinkedList<String> combos = new LinkedList<>();
 
         if(digits.length() == 0) {
-            return combo;
+            return combos;
         }
+        
+        // Initialise list with an empty string to begin with
+        combos.add("");
 
-        combo.add("");
-        while(combo.peek().length() != digits.length()) {
-            String prefix = combo.remove();
-            String suffix = map[digits.charAt(prefix.length()) - '0'];
+        // Keep creating combinations until combo length becomes equal to number of digits
+        while(combos.peek().length() != digits.length()) {
+            // Get the first string in list
+            String prefix = combos.remove();
+
+            // Append current digit's letters to first string - use prefix length to get index
+            String suffix = letters[digits.charAt(prefix.length()) - '0'];
             for(char c : suffix.toCharArray()) {
-                combo.add(prefix + c);
+                combos.add(prefix + String.valueOf(c));
             }
         }
 
-        return combo;
+        return combos;
     }
 }
+
+
 
 /**
  * Intuition - Start with alphabets from the first digit and add them to the result.
