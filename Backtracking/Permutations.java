eba37e9d8f83adcda582415b5225c1d91ed9681d
Vaishnavi Killekar <vaishnavikillekar@gmail.com>
2023-06-27 08:03:23 +0530

    Permutations - swap approach

--- Backtracking/Permutations.java
+++ Backtracking/Permutations.java
@@ -1,7 +1,48 @@
 // Link to problem - https://leetcode.com/problems/permutations/description/
 
 /**
- * Intuition - Use recursion with backtracking to generate all permutations.
+ * Intuition - Recursion with backtracking to generate all permutations.
+ * Start with tracking current element under consideration using 'index'.
+ * Swap the current element i with element at 'index' and recurse. Swapping
+ * elements will generate all permutations.
+ * When 'index' goes beyond array, then add current array as a permutation.
+ * On backtracking, undo the swap.
+ *
+ * Time complexity - O(n!) - n! permutations to be generated
+ * Space complexity - O(n)
+ */
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        LinkedList<List<Integer>> perms = new LinkedList<>();
+        int index = 0;
+
+        permute(nums, perms, index);
+        return perms;
+    }
+
+    public void permute(int[] nums, LinkedList<List<Integer>> perms, int index) {
+        if(index >= nums.length) {
+            // Add current permutation to result
+            perms.add(Arrays.stream(nums).boxed().toList());
+            return;
+        }
+
+        for(int i = index; i < nums.length; i++) {
+            swap(nums, i, index);               // Swap current and index
+            permute(nums, perms, index + 1);    // Find the next set of permutations
+            swap(nums, i, index);               // Undo swap
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+}
+
+/**
+ * Intuition - Recursion with backtracking to generate all permutations.
  *
  * Time complexity - O(n * n!) - n! permutations to be generated
  * Space complexity - O(n)
