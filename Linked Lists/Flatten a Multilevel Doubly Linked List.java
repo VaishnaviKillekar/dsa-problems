e4cff4df10e30bbbb8951251b2f3dbd7e2bb497a
Vaishnavi Killekar <vaishnavikillekar@gmail.com>
2023-07-07 20:06:05 +0530

    Flatten a Multilevel Linked List - optimal solution

--- Linked Lists/Flatten a Multilevel Doubly Linked List.java
+++ Linked Lists/Flatten a Multilevel Doubly Linked List.java
@@ -4,8 +4,48 @@
  * Intuition - Recursion 
  * Iterate through each node and recursively call the function again for every child.
  *
+ * Time complexity - O(n) - we iterate through all nodes next to each other and then through child nodes. 
+ * Space complexity - O(n) - maximum height of recursion tree when list has only child nodes
+ */
+class Solution {
+    public Node flatten(Node head) {
+        if(head != null) {
+            flattenAndReturnTail(head);
+        }
+        return head;
+    }
+
+    public Node flattenAndReturnTail(Node curr) {
+        Node tail = null;
+        while(curr != null) {
+            tail = curr;
+            Node next = curr.next;
+            if(curr.child != null) {
+                tail = flattenAndReturnTail(curr.child);
+                // Link child's tail
+                tail.next = next;
+                if(next != null) {
+                    next.prev = tail;
+                }
+                // Link child's head (i.e. curr.child)
+                curr.next = curr.child;
+                curr.child.prev = curr;
+                curr.child = null;
+            }
+            curr = next;
+        }
+        return tail;
+    }
+}
+
+
+
+/**
+ * Intuition - Recursion 
+ * Iterate through each node and recursively call the function again for every child.
+ *
  * Time complexity - O(n^2) - we iterate through all nodes next to each other and then through child nodes. 
- *                            Then we iterate through children again to find tail. Worst case, each node has child nodes and finding tail could cost n. Confirm?
+ *                            Then we iterate through children again to find tail. Worst case, each node has child nodes and finding tail could cost n.
  * Space complexity - O(n) - maximum height of recursion tree when list has only child nodes
  */
 class Solution {
