0aa644ff55d7f9ce2491fa5befb3cd7cbc7cb665
Vaishnavi Killekar <vaishnavikillekar@gmail.com>
2023-09-10 10:01:41 +0530

    Left View of Binary Tree without hashmap

--- Trees/Left View of Binary Tree.java
+++ Trees/Left View of Binary Tree.java
@@ -1,12 +1,56 @@
 // Link to problem - https://practice.geeksforgeeks.org/problems/left-view-of-binary-tree/1
 
 /**
+ * Intuition - Use recursion to traverse the tree and every time a new row
+ * is visited, add the first node in result since the first found node alone
+ * will be visible from the left view.
+ * At every level/row, we add the node only when it is entered first time i.e.,
+ * when the number of nodes in result matches the row level. Every other node
+ * in that row is ignored.
+ *
+ * Time complexity - O(n)
+ * Space complexity - O(n) - recursion
+ */
+class Tree
+{
+    ArrayList<Integer> leftView(Node root)
+    {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        if(root == null) {
+            return result;
+        }
+        
+        // Get the first node for each row
+        traverse(root, result, 0);
+        
+        return result;
+    }
+    
+    void traverse(Node node, ArrayList<Integer> result, int row) {
+        if(node == null) {
+            return;
+        }
+        
+        // Is this row discovered?
+        if(result.size() == row) {
+            // Row found for the first time
+            result.add(node.data);
+        }
+        
+        traverse(node.left, result, row + 1);
+        traverse(node.right, result, row + 1);
+    }
+}
+
+
+/**
  * Intuition - Use a HashMap to store every unique row and the leftmost
  * node in it i.e., the first found node as it will be the only visible 
  * node from the left.
  *
  * Time complexity - O(n)
- * Space complexity - O(n)
+ * Space complexity - O(n) - recursion + HashMap
  */
 class Tree
 {
